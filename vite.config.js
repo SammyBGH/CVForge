0c6cded29fba254d290e96fdf8a87cd255429e32
Samuel <sammylemon69@gmail.com>
2025-10-22 15:15:53 +0000

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          vendor: ['html2canvas', 'jspdf', 'react-icons']
+        }
+      }
+    }
+  },
+  preview: {
+    port: 3000
+  }
 })
